7849eeaf265d335e1f03066bfde34dffe9416977
nguyentuandung93 <tuandungbk56@gmail.com>
2020-05-15 14:30:15 +0900

    configファイルを修正

--- TodoApp/src/main/java/app/controller/TodoController.java
+++ TodoApp/src/main/java/app/controller/TodoController.java
@@ -1,17 +1,12 @@
 package app.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,6 +23,7 @@ public class TodoController {
 	
 	@Autowired
 	private TodoService todoService;
+	
 	// Todo Index画面
 	@RequestMapping(value = "/todo", method = {RequestMethod.GET, RequestMethod.POST})
 	public String index() {
