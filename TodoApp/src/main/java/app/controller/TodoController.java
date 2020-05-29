773abb803a283ff511df34ec1735513cbea8bcf0
nguyentuandung93 <tuandungbk56@gmail.com>
2020-05-29 19:09:43 +0900

    Todoappを修正

--- TodoApp/src/main/java/app/controller/TodoController.java
+++ TodoApp/src/main/java/app/controller/TodoController.java
@@ -1,22 +1,17 @@
 package app.controller;
 
-import java.util.Enumeration;
 import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import app.model.Todo;
